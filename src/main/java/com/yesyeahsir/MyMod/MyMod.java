37885e77fa17da4c3dda9e095bde26b5a395b757
YesYeahSir <118585907+YesYeahSir@users.noreply.github.com>
2023-03-22 12:30:43 +1300

    Added new temporary Items, ModItems Class, added more comments, and added an Assets Folder.

--- src/main/java/com/yesyeahsir/MyMod/MyMod.java
+++ src/main/java/com/yesyeahsir/MyMod/MyMod.java
@@ -1,9 +1,12 @@
 package com.yesyeahsir.MyMod;
 
 import com.mojang.logging.LogUtils;
+import com.yesyeahsir.MyMod.item.ModItems;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -12,25 +15,40 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// This is the main mod class
+// Main Mod Class
 @Mod(MyMod.MOD_ID)
 public class MyMod
 {
-    // This is the Mod ID
+    // The Mod ID
     public static final String MOD_ID = "mymod";
     private static final Logger LOGGER = LogUtils.getLogger();
+
+    // Register Custom Items/Blocks
     public MyMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        modEventBus.addListener(this::commonSetup);
+        ModItems.Register(modEventBus);
 
+        modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
 
+    }
+
+    // Add Items to Creative Tabs
+    private void addCreative(CreativeModeTabEvent.BuildContents event)
+    {
+        if (event.getTab() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.Emeraldus);
+            event.accept(ModItems.Fake_Book);
+        }
     }
 
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
---
+++ src/main/java/com/yesyeahsir/MyMod/item/ModItems.java
@@ -0,0 +1,26 @@
+package com.yesyeahsir.MyMod.item;
+
+import com.yesyeahsir.MyMod.MyMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+// The main Custom Items Class
+public class ModItems
+{
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MyMod.MOD_ID);
+
+    public static final RegistryObject<Item> Emeraldus =
+            ITEMS.register("emeraldus", () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> Fake_Book =
+            ITEMS.register("fake_book", () -> new Item(new Item.Properties()));
+
+    public static void Register(IEventBus eventBus)
+    {
+        ITEMS.register(eventBus);
+    }
+}
